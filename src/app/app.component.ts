b8cef9a9fc72370aab79ab38f67c1197d5495849
unknown <pragya18067@iiitd.ac.in>
2020-06-28 18:57:11 +0530

    add function to put X or O on click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mars-Colonization-Program';
+  rows = [0, 1, 2];
+  columns = [0, 1, 2];
+  gridValue : number[][] = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ];
+  gridContent : string[][] = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+  ];
+  turn = 0;
+
+  constructor(){
+    for(let row = 0; row < 3; row ++){
+      for (let col = 0; col < 3; col ++){
+        this.gridValue[row][col] = 0;
+        this.gridContent[row][col] = " ";
+      }
+    }
+  }
+
+  setValue(row, col){
+    if (this.turn == 0){
+        this.gridContent[row][col] = "X";
+    }
+    else{
+        this.gridContent[row][col] = "O";
+    }
+    this.turn = 1 - this.turn;
+  }
 }
+
+
+
+
