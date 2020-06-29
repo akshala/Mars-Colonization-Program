2a21799739ae58aeb48cae772358acb57911b29a
Vrinda Narayan <vrinda18120@iiitd.ac.in>
2020-06-30 00:13:38 +0530

    Added new Board

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -9,6 +9,7 @@ import { CellEnum } from '../cell/CellEnum';
 export class BoardComponent implements OnInit {
   private currentPlayer: CellEnum;
   public board: CellEnum[][];
+  private myBoard: CellEnum[][];
   private isGameOver: boolean;
   public statusMessage;
 
@@ -24,10 +25,13 @@ export class BoardComponent implements OnInit {
 
   newGame() {
     this.board = [];
+    this.myBoard = [];
     for (let row=0; row<3; row++) {
       this.board[row] = [];
+      this.myBoard[row] = [];
       for(let col=0; col<3; col++) {
         this.board[row][col] = CellEnum.EMPTY;
+        this.myBoard[row][col] = CellEnum.EMPTY;
       }
     }
     this.currentPlayer = CellEnum.x;
@@ -38,6 +42,7 @@ export class BoardComponent implements OnInit {
   move(row: number, col: number): void {
     if(!this.isGameOver && this.board[row][col] === CellEnum.EMPTY) {
       this.board[row][col] = this.currentPlayer;
+      this.myBoard[row][col] = this.currentPlayer;
       if(this.isDraw()) {
         this.statusMessage = 'It\'s a Draw!';
         this.isGameOver = true;
@@ -52,11 +57,12 @@ export class BoardComponent implements OnInit {
         this.currentPlayer = this.currentPlayer === CellEnum.x ? CellEnum.o : CellEnum.x;
       }
     }
-    this.AImove(this.currentPlayer, 3);
+    //let robot = this.currentPlayer === CellEnum.x ? CellEnum.o : CellEnum.x;
+    //this.AImove(robot, 3);
   }
 
   isDraw(): boolean {
-    for(const columns of this.board) {
+    for(const columns of this.myBoard) {
       for(const col of columns) {
         if(col === CellEnum.EMPTY) {
           return false;
@@ -68,27 +74,27 @@ export class BoardComponent implements OnInit {
 
   isWin(): boolean {
     //horizontal
-    return this.playerWon('X') || this.playerWon('O');
+    return this.playerWon(CellEnum.x) || this.playerWon(CellEnum.o);
   }
 
-  playerWon(player: String): boolean{
+  playerWon(player: CellEnum): boolean{
     // horizontal
-    for(const columns of this.board) {
+    for(const columns of this.myBoard) {
       if(columns[0] === columns[1] && columns[0] === columns[2] && columns[0] === player) {
         return true;
       }
     }
     // vertical
-    for(let col=0; col<this.board[0].length; col++) {
-      if (this.board[0][col] === this.board[1][col] && this.board[0][0] === this.board[2][col] && this.board[0][col] === player) {
+    for(let col=0; col<this.myBoard[0].length; col++) {
+      if (this.myBoard[0][col] === this.myBoard[1][col] && this.myBoard[0][0] === this.myBoard[2][col] && this.myBoard[0][col] === player) {
         return true;
       }
     }
     // diagonal
-    if(this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2] && this.board[0][0] === player) {
+    if(this.myBoard[0][0] === this.myBoard[1][1] && this.myBoard[0][0] === this.myBoard[2][2] && this.myBoard[0][0] === player) {
       return true;
     }
-    if(this.board[0][2] === this.board[1][1] && this.board[0][2] == this.board[2][0] && this.board[0][2] === player) {
+    if(this.myBoard[0][2] === this.myBoard[1][1] && this.myBoard[0][2] == this.myBoard[2][0] && this.myBoard[0][2] === player) {
       return true;
     }
     return false;
@@ -101,24 +107,24 @@ export class BoardComponent implements OnInit {
     }
   }
 
-  AImove(player: CellEnum, difficulty: number) : number[]{
+  AImove(player: CellEnum, difficulty: number) : void{
     var cells: number[][] = [[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]];
     this.shuffle(cells);
     var depths: number[] = [2,4,6,1000];
 
-    if(player === 'X'){
+    if(player === CellEnum.x){
       var r: number = -1;
       var c: number = -1;
       var best: number = -10;
       for(const points of cells){
         let x = points[0];
         let y = points[1];
-        if(this.board[x][y] !== CellEnum.EMPTY){
+        if(this.myBoard[x][y] !== CellEnum.EMPTY){
           continue;
         }
-        this.board[x][y] = CellEnum.x;
+        this.myBoard[x][y] = CellEnum.x;
         let temp = this.getScore(CellEnum.o, depths[difficulty]);
-        this.board[x][y] = CellEnum.EMPTY;
+        this.myBoard[x][y] = CellEnum.EMPTY;
         if(temp>best){
           best = temp;
           r = x;
@@ -132,12 +138,12 @@ export class BoardComponent implements OnInit {
       for(const points of cells){
         let x = points[0];
         let y = points[1];
-        if(CellEnum.EMPTY !== CellEnum.EMPTY){
+        if(this.myBoard[x][y] !== CellEnum.EMPTY){
           continue;
         }
-        this.board[x][y] = CellEnum.o;
+        this.myBoard[x][y] = CellEnum.o;
         let temp = this.getScore(CellEnum.x, depths[difficulty]);
-        this.board[x][y] = CellEnum.EMPTY;
+        this.myBoard[x][y] = CellEnum.EMPTY;
         if(temp<best){
           best = temp;
           r = x;
@@ -145,32 +151,32 @@ export class BoardComponent implements OnInit {
         }
       }
     }
-    var ans: number[] = [r,c];
-    return ans;
+    this.board[r][c] = player;
+    this.myBoard[r][c] = player;
   }
 
   getScore(player : CellEnum,  depth: number): number {
     if(this.isDraw()){
         return 0;
     }
-    if(this.playerWon('X')){
+    if(this.playerWon(CellEnum.x)){
       return 1;
     }
-    if(this.playerWon('O')){
+    if(this.playerWon(CellEnum.o)){
       return -1;
     }
 
-    if(player==='X'){
+    if(player===CellEnum.x){
       if(depth===0)
         return -10;
       //maximizing 
       var best:number = -10;
       for(let row = 0; row<3; row++){
         for(let col = 0; col<3; col++){
-          if(this.board[row][col]===CellEnum.EMPTY){
-            this.board[row][col] = CellEnum.x;
+          if(this.myBoard[row][col]===CellEnum.EMPTY){
+            this.myBoard[row][col] = CellEnum.x;
             best = Math.max(this.getScore(CellEnum.o, depth-1), best);
-            this.board[row][col] = CellEnum.EMPTY;
+            this.myBoard[row][col] = CellEnum.EMPTY;
           }
         }
 
@@ -183,10 +189,10 @@ export class BoardComponent implements OnInit {
       var best:number = 10;
       for(let row = 0; row < 3; row++){
         for(let col = 0; col < 3; col++){
-          if(this.board[row][col]===CellEnum.EMPTY){
-            this.board[row][col] = CellEnum.o;
+          if(this.myBoard[row][col]===CellEnum.EMPTY){
+            this.myBoard[row][col] = CellEnum.o;
             best = Math.min(this.getScore(CellEnum.x, depth-1), best);
-            this.board[row][col] = CellEnum.EMPTY;
+            this.myBoard[row][col] = CellEnum.EMPTY;
           }
         }
 
