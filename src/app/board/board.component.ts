62f1ab2decd8d54b14c1d37792a32c430fe6729f
Pragyan Mehrotra <pragyan18168@iiitd.ac.in>
2020-06-29 23:39:37 +0530

    Coded Minimax algorithm

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -41,15 +41,18 @@ export class BoardComponent implements OnInit {
       if(this.isDraw()) {
         this.statusMessage = 'It\'s a Draw!';
         this.isGameOver = true;
+        return;
       }
       else if(this.isWin()) {
           this.statusMessage = 'player $(this.currentPlayer) won!';
           this.isGameOver = true;
+          return;
       }
       else {
         this.currentPlayer = this.currentPlayer === CellEnum.x ? CellEnum.o : CellEnum.x;
       }
     }
+    this.AImove(this.currentPlayer, 3);
   }
 
   isDraw(): boolean {
@@ -65,66 +68,89 @@ export class BoardComponent implements OnInit {
 
   isWin(): boolean {
     //horizontal
-    for(const columns of this.board) {
-      if(columns[0] === columns[1] && columns[0] === columns[2] && columns[0] !== CellEnum.EMPTY) {
-        return true;
-      }
-    }
-    // vertical
-    for(let col=0; col<this.board[0].length; col++) {
-      if (this.board[0][col] === this.board[1][col] && this.board[0][0] === this.board[2][col] && this.board[0][col] !== CellEnum.EMPTY) {
-        return true;
-      }
-    }
-    // diagonal
-    if(this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2] && this.board[0][0] !== CellEnum.EMPTY) {
-      return true;
-    }
-    if(this.board[0][2] === this.board[1][1] && this.board[0][2] == this.board[2][0] && this.board[0][2] !== CellEnum.EMPTY) {
-      return true;
-    }
-    return false;
+    return this.playerWon('X') || this.playerWon('O');
   }
 
-  playerWon(s: String): boolean{
+  playerWon(player: String): boolean{
     // horizontal
     for(const columns of this.board) {
-      if(columns[0] === columns[1] && columns[0] === columns[2] && columns[0] === s) {
+      if(columns[0] === columns[1] && columns[0] === columns[2] && columns[0] === player) {
         return true;
       }
     }
     // vertical
     for(let col=0; col<this.board[0].length; col++) {
-      if (this.board[0][col] === this.board[1][col] && this.board[0][0] === this.board[2][col] && this.board[0][col] === s) {
+      if (this.board[0][col] === this.board[1][col] && this.board[0][0] === this.board[2][col] && this.board[0][col] === player) {
         return true;
       }
     }
     // diagonal
-    if(this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2] && this.board[0][0] === s) {
+    if(this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2] && this.board[0][0] === player) {
       return true;
     }
-    if(this.board[0][2] === this.board[1][1] && this.board[0][2] == this.board[2][0] && this.board[0][2] === s) {
+    if(this.board[0][2] === this.board[1][1] && this.board[0][2] == this.board[2][0] && this.board[0][2] === player) {
       return true;
     }
     return false;
   }
 
-  AImove() : void{
-    
+  shuffle(array: number[][]): void{
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
   }
 
-  moveLeft(): boolean {
-    for(let row = 0; row<3; row++){
-      for(let col = 0; col<3; col++){
-        if(this.board[row][col]===CellEnum.EMPTY){
-          return true;
+  AImove(player: CellEnum, difficulty: number) : number[]{
+    var cells: number[][] = [[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]];
+    this.shuffle(cells);
+    var depths: number[] = [2,4,6,1000];
+
+    if(player === 'X'){
+      var r: number = -1;
+      var c: number = -1;
+      var best: number = -10;
+      for(const points of cells){
+        let x = points[0];
+        let y = points[1];
+        if(this.board[x][y] !== CellEnum.EMPTY){
+          continue;
+        }
+        this.board[x][y] = CellEnum.x;
+        let temp = this.getScore(CellEnum.o, depths[difficulty]);
+        this.board[x][y] = CellEnum.EMPTY;
+        if(temp>best){
+          best = temp;
+          r = x;
+          c = y;
+        }
+      }
+    }else{
+      var r: number = -1;
+      var c: number = -1;
+      var best: number = 10;
+      for(const points of cells){
+        let x = points[0];
+        let y = points[1];
+        if(CellEnum.EMPTY !== CellEnum.EMPTY){
+          continue;
+        }
+        this.board[x][y] = CellEnum.o;
+        let temp = this.getScore(CellEnum.x, depths[difficulty]);
+        this.board[x][y] = CellEnum.EMPTY;
+        if(temp<best){
+          best = temp;
+          r = x;
+          c = y;
         }
       }
     }
-    return false;
+    var ans: number[] = [r,c];
+    return ans;
   }
-  bestMove(): number {
-    if(!this.moveLeft()){
+
+  getScore(player : CellEnum,  depth: number): number {
+    if(this.isDraw()){
         return 0;
     }
     if(this.playerWon('X')){
@@ -134,6 +160,39 @@ export class BoardComponent implements OnInit {
       return -1;
     }
 
+    if(player==='X'){
+      if(depth===0)
+        return -10;
+      //maximizing 
+      var best:number = -10;
+      for(let row = 0; row<3; row++){
+        for(let col = 0; col<3; col++){
+          if(this.board[row][col]===CellEnum.EMPTY){
+            this.board[row][col] = CellEnum.x;
+            best = Math.max(this.getScore(CellEnum.o, depth-1), best);
+            this.board[row][col] = CellEnum.EMPTY;
+          }
+        }
+
+      }
+      return best;
+    }else{
+      if(depth===0)
+        return 10;
+      //maximizing 
+      var best:number = 10;
+      for(let row = 0; row < 3; row++){
+        for(let col = 0; col < 3; col++){
+          if(this.board[row][col]===CellEnum.EMPTY){
+            this.board[row][col] = CellEnum.o;
+            best = Math.min(this.getScore(CellEnum.x, depth-1), best);
+            this.board[row][col] = CellEnum.EMPTY;
+          }
+        }
+
+      }
+      return best;
+    }
   }
 
 }
